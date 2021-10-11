d22c170e4841b5c5ee6c9e3f8afd5872239e168f
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2021-10-11 19:54:07 +0100

    create a spark submit call

---
+++ consumer/submit_spark_job.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/spark-submit  --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 /Users/mihhailf/python/mind-tricks-technologies/data-trading-server/consumer/consumer/main.py
