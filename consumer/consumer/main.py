265667a155f5fd43c5abed4feca9c5cb749c4f65
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2021-10-11 22:08:48 +0100

    spark consumer parses the kafka message contents to a dataframe

--- consumer/consumer/main.py
+++ consumer/consumer/main.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import StructType, DecimalType, TimestampType, StringType
 
 if __name__ == '__main__':
 
@@ -10,23 +12,29 @@ if __name__ == '__main__':
              .config("spark.executor.memory", "512m")
              .getOrCreate())
 
-    # spark = (SparkSession.builder
-    #          # .config("spark.master", "spark://0.0.0.0:7077")
-    #          # .config("spark.driver.host", "192.168.0.13")
-    #          # .config("spark.submit.deployMode", "client")
-    #          # .config("spark.driver.bindAddress", "192.168.0.13")
-    #          # .config("spark.executor.memory", "512m")
-    #          .getOrCreate())
+    # define a schema for the value returned from Kafka
+    schema = (StructType()
+              .add("bid", DecimalType())
+              .add("ask", DecimalType())
+              .add("pair", StringType())
+              .add("timestamp", TimestampType()))
 
+    # start streaming data from the kafka broker
     df = (spark
           .readStream
           .format("kafka")
           .option("kafka.bootstrap.servers", "0.0.0.0:9094,kafka:9092")
           .option("subscribe", "price_changes1")
+          .option("startingOffsets", "latest")
           .load())
 
-    (df.writeStream
-        .format("console")
-        .outputMode("append")
-        .start()
-        .awaitTermination())
+    # parse kafka message value and convert it to a data frame
+    df_parsed = (df.withColumn("value", from_json(col("value").cast("string"), schema))
+                 .select(col("value.*")))
+
+    # print output to the console
+    (df_parsed.writeStream
+     .format("console")
+     .outputMode("append")
+     .start()
+     .awaitTermination())
